741924e8467d25eb1384f352f4d5d9961f32d403
Alejandro <ale11199@estudiantec.cr>
2025-01-20 16:27:44 -0600

    Refactor del main para separar la ejecución del lexer y parser a métodos aislados
    Esto se hace con el objetivo de que el método main sea más ordenado y claro.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,13 @@
 package main.java;
 
 import java_cup.runtime.*;
+
 import java.util.Objects;
+
 import main.java.sym;
 import main.java.Parser;
 import main.java.LexerCupV;
+
 import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -24,57 +27,57 @@ public class Main {
                 System.err.println("Error: El archivo especificado no existe o no es válido.");
                 return;
             }
-
             System.out.println("Archivo de entrada encontrado: " + inputFile.getAbsolutePath());
 
-            Path outputPath = Paths.get("output.txt").toAbsolutePath();
-            File outputFile = outputPath.toFile();
+            // aquí se llaman los métodos para correr cada "etapa" del compilador
+            runLexer(inputFile);
+            runParser(inputFile);
 
-            System.out.println("Se creará el archivo de salida en: " + outputFile.getAbsolutePath());
-
-            try {
-                LexerCupV lexer = new LexerCupV(new FileReader(inputFile));
-                BufferedWriter writer = new BufferedWriter(new FileWriter("output_lexer.txt"));
-
-                Symbol token;
-
-                while (!Objects.isNull(token = lexer.next_token())) {
-                    if (token.sym == sym.EOF) break;
-                    String tokenInfo = lexer.getTokenInfo(token.sym);
-                    writer.write(tokenInfo);
-                    writer.newLine();
-                }
-                writer.close();
-                System.out.println("\n\nAnálisis léxico completado. Se escribió la salida al archivo: output_lexer.txt");
-
-                // probando el parseo con un lexer nuevo
-                System.out.println("--------------------------");
-                System.out.println("Iniciando análisis sintáctico:");
-
-                lexer = new LexerCupV(new FileReader(inputFile));
-                writer = new BufferedWriter(new FileWriter("output_parser.txt"));
-                BufferedWriter symTable = new BufferedWriter(new FileWriter("output_symTable.txt"));
-
-                Parser parser = new Parser(lexer, writer, symTable);
-                parser.parse();
-                writer.close();
-                //parser.printSymbolTable();
-                symTable.close();
-                System.out.println("Se escribió la tabla de símbolos al archivo output_symTable.txt y los errores encontrados al output_parser.txt");
-                System.out.println();
-
-
-                File file = new File("output_parser.txt");
-                System.out.println((file.length() == 0) ? "sí se puede producir el archivo con la gramática" : "hay errores, no se podría producir el archivo con la gramática.");
-
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-            //esto es un comentario de prueba
-            //esto es otro comentario de prueba
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
+    }
+
+
+    private static void runLexer(File inputSource) {
+        try {
+            LexerCupV lexer = new LexerCupV(new FileReader(inputSource));
+            BufferedWriter writer = new BufferedWriter(new FileWriter("output_lexer.txt"));
+
+            Symbol token;
+
+            while (!Objects.isNull(token = lexer.next_token())) {
+                if (token.sym == sym.EOF) break;
+                String tokenInfo = lexer.getTokenInfo(token.sym);
+                writer.write(tokenInfo);
+                writer.newLine();
+            }
+            writer.close();
+            System.out.println("\n\nAnálisis léxico completado. Se escribió la salida al archivo: output_lexer.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void runParser(File inputSource) throws Exception {
+        // probando el parseo con un lexer nuevo
+        System.out.println("--------------------------");
+        System.out.println("Iniciando análisis sintáctico:");
+
+        LexerCupV lexer = new LexerCupV(new FileReader(inputSource));
+        BufferedWriter writer = new BufferedWriter(new FileWriter("output_parser.txt"));
+        BufferedWriter symTable = new BufferedWriter(new FileWriter("output_symTable.txt"));
+
+        Parser parser = new Parser(lexer, writer, symTable);
+        parser.parse();
+        writer.close();
+        //parser.printSymbolTable();
+        symTable.close();
+        System.out.println("Se escribió la tabla de símbolos al archivo output_symTable.txt y los errores encontrados al output_parser.txt");
+        System.out.println();
+
+
+        File file = new File("output_parser.txt");
+        System.out.println((file.length() == 0) ? "sí se puede producir el archivo con la gramática" : "hay errores, no se podría producir el archivo con la gramática.");
     }
 }
